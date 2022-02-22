27f0eba6bf24d029082b8f96d1fe8e9436d6aeea
tim-au <99239974+tim-au@users.noreply.github.com>
2022-02-23 01:02:07 +1030

    Update __init__.py

--- plutoserver/__init__.py
+++ plutoserver/__init__.py
@@ -5,10 +5,9 @@ def setup_plutoserver():
     mydir = os.path.dirname(os.path.realpath(__file__))
     iconpath = os.path.join(mydir, "icons", "pluto-logo.svg")
     return {
-        "command": ["/bin/bash", "runpluto.sh", "{port}"],
+        "command": ["/bin/bash", "/home/jovyan/runpluto.sh", "{port}"],
         "timeout": 60,
         "launcher_entry": {"title": "Pluto.jl", "icon_path": iconpath},
     }
 
-# note - I deleted this:
-# "command": ["/bin/bash", "/home/jovyan/runpluto.sh", "{port}"]
+
