b5b81abfa58142703131a98668e2a476b2e8fcfb
tim-au <99239974+tim-au@users.noreply.github.com>
2022-02-23 00:20:17 +1030

    Update create_sysimage.jl

--- create_sysimage.jl
+++ create_sysimage.jl
@@ -1,5 +1,5 @@
 using PackageCompiler
-create_sysimage([:Pluto, :PlutoUI, :Distributions, :StatsPlots];
+create_sysimage([:Pluto, :PlutoUI, :Plots, :LaTeXStrings, :Unitful, :DataFrames, :LinearAlgebra];
                 precompile_execution_file = "warmup.jl",
                 replace_default = true,
                 cpu_target = PackageCompiler.default_app_cpu_target())
